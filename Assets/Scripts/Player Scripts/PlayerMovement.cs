dac477fc0a4010f9b565b2d3a841d4d06436bb2e
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-02-01 20:17:29 +0000

    Basic Keyboard Controls
    Basic keyboard controls added

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //setting up public variables
+    public Vector2 directionAiming;
+    private Rigidbody2D rb;
+    public float speed = 10.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+    {
+
+        directionAiming = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        rb.velocity = Vector2.zero;
+        //pulls controls from unity input manager 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.velocity += new Vector2(horizontal, vertical) * speed;
+
+
+
+        if (Input.GetButton("Attack"))
+        {
+            //insert attack code here
+
+        }
+    }
+}
