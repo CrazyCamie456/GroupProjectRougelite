4b0d64d9a899b04d24ef69993a5796c3584ebcf1
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-05 12:00:38 +0000

    Implementing ElementAttack
    This component allows the player to attack with their currently equipped element. Currently, the element they have equipped cannot be changed at runtime - see line 37.

---
+++ Assets/Scripts/Player Scripts/Elemental Attack Scripts/ElementAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementAttack : MonoBehaviour
+{
+	public IElementalAttack elementalAttack;
+
+	public float projectileSpeed = 10.0f;
+	// In attacks per second.
+	public float baseAttackSpeed = 1.0f;
+
+	// Runtime variables
+	float fireDelay = 0.0f;
+	float currDelay = 0.0f;
+
+	PlayerMovement playerMovement;
+	PlayerController playerController;
+
+	private void Awake()
+	{
+		playerController = new PlayerController();
+	}
+
+	private void OnEnable()
+	{
+		playerController.Enable();
+	}
+	private void OnDisable()
+	{
+		playerController.Disable();
+	}
+
+	void Start()
+	{
+		playerMovement = GetComponent<PlayerMovement>();
+		elementalAttack = gameObject.AddComponent<ElementWaterAttack>();
+		baseAttackSpeed = elementalAttack.GetBaseAttackSpeed();
+	}
+
+	void Update()
+	{
+		fireDelay = 1.0f / baseAttackSpeed;
+
+		// If the attack button is down, and the player is aiming in any direction, and the players attack is off cooldown.
+		if (playerController.Player.Attack.ReadValue<float>() > 0.5f &&
+			(Mathf.Abs(playerMovement.directionAiming.x) > 0.1f || Mathf.Abs(playerMovement.directionAiming.y) > 0.1f)
+			&& currDelay <= 0.0f)
+		{
+			currDelay = fireDelay;
+			elementalAttack.Attack(playerMovement.directionAiming);
+		}
+		currDelay -= Time.deltaTime;
+	}
+}
