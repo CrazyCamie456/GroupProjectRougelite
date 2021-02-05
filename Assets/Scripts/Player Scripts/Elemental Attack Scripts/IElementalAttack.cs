11a2d55f1fb33435b40e864ca02776fecae3a53a
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-05 11:51:39 +0000

    Implement Interface for elemental attacks
    This interface should be used by all elemental attacks. This allows the creation of new elemental attacks to be faster, and encapsulates all of their behaviour within their own class.

---
+++ Assets/Scripts/Player Scripts/Elemental Attack Scripts/IElementalAttack.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IElementalAttack
+{
+	string Name();
+	void Attack(Vector2 direction);
+	float GetBaseAttackSpeed();
+}
