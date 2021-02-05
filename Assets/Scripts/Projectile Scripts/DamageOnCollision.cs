93d1adde0d1ce530b7c7823aeeb71bbcc61e6127
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-05 11:53:08 +0000

    Adding basic projectile behaviours
    Implementing two fairly simple projectile behaviour classes, which should speed up making new projectiles in future.

---
+++ Assets/Scripts/Projectile Scripts/DamageOnCollision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnCollision : MonoBehaviour
+{
+	
+	public List<string> targetLayers;
+	public int damage;
+
+	public void Initialise(string _targetLayer, int _damage)
+	{
+		targetLayers = new List<string>() { _targetLayer };
+		damage = _damage;
+	}
+
+	public void Initialise(List<string> _targetLayers, int _damage)
+	{
+		targetLayers = _targetLayers;
+		damage = _damage;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (targetLayers.Contains(collider.tag))
+		{
+			collider.GetComponent<EnemyHealth>().TakeDamage(damage);
+		}
+	}
+}
---
+++ Assets/Scripts/Projectile Scripts/DestroySelfOnCollision.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroySelfOnCollision : MonoBehaviour
+{
+	
+	public List<string> targetLayers;
+
+	public void Initialise(string _targetLayer)
+	{
+		targetLayers = new List<string>() { _targetLayer };
+	}
+
+	public void Initialise(List<string> _targetLayers)
+	{
+		targetLayers = _targetLayers;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (targetLayers.Contains(collider.tag))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
