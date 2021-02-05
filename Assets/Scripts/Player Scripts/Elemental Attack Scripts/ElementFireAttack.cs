fbdf1a874ea04b1001f821fc0b316c5c1bb9575c
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-05 11:57:03 +0000

    Implementing first two elements
    These two scripts create and fire their respective attacks, dealing damage to enemies within the new "Enemy" tag, and destroying themselves on collision.

---
+++ Assets/Scripts/Player Scripts/Elemental Attack Scripts/ElementFireAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementFireAttack : MonoBehaviour, IElementalAttack
+{
+	// Compile time semi-static variables.
+	GameObject fireballPrefab;
+	float projectileSpeed = 10.0f;
+	float attackSpeed = 2.0f;
+
+	void Start()
+	{
+		fireballPrefab = Resources.Load<GameObject>("Prefabs/Fireball");
+	}
+
+	public void Attack(Vector2 direction)
+	{
+		GameObject fireball = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+		fireball.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+		fireball.GetComponent<DamageOnCollision>().Initialise("Enemy", 2);
+		fireball.GetComponent<DestroySelfOnCollision>().Initialise("Enemy");
+	}
+
+	public float GetBaseAttackSpeed() { return attackSpeed; }
+
+	public string Name() { return "Fire"; }
+}
---
+++ Assets/Scripts/Player Scripts/Elemental Attack Scripts/ElementWaterAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementWaterAttack : MonoBehaviour, IElementalAttack
+{
+	// Compile time semi-static variables.
+	GameObject waterShotPrefab;
+	float projectileSpeed = 8.0f;
+	float attackSpeed = 1.25f;
+
+	void Start()
+	{
+		waterShotPrefab = Resources.Load<GameObject>("Prefabs/Watershot");
+	}
+
+	public void Attack(Vector2 direction)
+	{
+		GameObject waterShot = Instantiate(waterShotPrefab, transform.position, Quaternion.identity);
+		waterShot.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
+		waterShot.GetComponent<DamageOnCollision>().Initialise("Enemy", 3);
+		waterShot.GetComponent<DestroySelfOnCollision>().Initialise("Enemy");
+	}
+
+	public float GetBaseAttackSpeed() { return attackSpeed; }
+
+	public string Name() { return "Water"; }
+}
