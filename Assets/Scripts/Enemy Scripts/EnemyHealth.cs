e8d2e4b2aa116212692acac2e3cfa034bba04b2a
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-05 12:02:28 +0000

    Adding Enemy Health
    Giving the enemies a basic understanding of health, meaning they can take damage and die.

---
+++ Assets/Scripts/Enemy Scripts/EnemyHealth.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+	public int health;
+	public int maxHealth;
+
+	public void Initialise(int _maxHealth)
+	{
+		maxHealth = _maxHealth;
+		health = maxHealth;
+	}
+
+	public void Initialise(int _health, int _maxHealth)
+	{
+		health = _health;
+		maxHealth = _maxHealth;
+	}
+
+	public void TakeDamage(int damage)
+	{
+		health -= damage;
+		if (health <= 0) Destroy(gameObject);
+	}
+
+	void Start()
+    {
+		health = maxHealth;
+    }
+
+	void Update()
+	{
+		
+	}
+}
