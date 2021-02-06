96540ca83a13a317076e13244d59bdd8d5feeb61
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-06 21:57:06 +0000

    Added slow on hit
    Added a script that allows a projectile to apply a slow to an enemy on collision.

---
+++ Assets/Scripts/Projectile Scripts/SlowTargetOnHit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTargetOnHit : MonoBehaviour
+{
+
+	public List<string> targetLayers;
+	public float slowPotency;
+	public float slowDuration;
+
+	public void Initialise(string _targetLayer, float _slowPotency, float _slowDuration)
+	{
+		targetLayers = new List<string>() { _targetLayer };
+		slowPotency = _slowPotency;
+		slowDuration = _slowDuration;
+	}
+
+	public void Initialise(List<string> _targetLayers, float _slowPotency, float _slowDuration)
+	{
+		targetLayers = _targetLayers;
+		slowPotency = _slowPotency;
+		slowDuration = _slowDuration;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (targetLayers.Contains(collider.tag))
+		{
+			collider.gameObject.AddComponent<Slow>().Initialise(slowPotency, slowDuration);
+		}
+	}
+}
