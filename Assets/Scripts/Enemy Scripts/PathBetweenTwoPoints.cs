a33326f33f1389b22998cc01fcd85e6ff21fa41d
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-06 21:45:47 +0000

    Make an enemy move between points A and B
    This is mostly for debugging, since there isn't any pathfinding between the two.

---
+++ Assets/Scripts/Enemy Scripts/PathBetweenTwoPoints.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathBetweenTwoPoints : MonoBehaviour
+{
+	public Vector2 pointA;
+	public Vector2 pointB;
+	bool movingTowardsB;
+	Rigidbody2D rb;
+	CombatStats cs;
+
+	void Start()
+	{
+		movingTowardsB = true;
+		cs = GetComponent<CombatStats>();
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		if (movingTowardsB)
+		{
+			if (Vector2.Distance(rb.position, pointB) < 0.2f)
+			{
+				movingTowardsB = !movingTowardsB;
+			}
+			rb.velocity = (pointB - rb.position).normalized * cs.movementSpeed;
+		}
+		else
+		{
+			if (Vector2.Distance(rb.position, pointA) < 0.2f)
+			{
+				movingTowardsB = !movingTowardsB;
+			}
+			rb.velocity = (pointA - rb.position).normalized * cs.movementSpeed;
+		}
+	}
+}
