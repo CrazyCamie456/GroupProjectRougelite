ffe0c3db8c778cdc294b60530ce61fe4e7af0842
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-06 21:55:45 +0000

    Added Slow
    Added a slow debuff that will decrease the movement speed of whichever entity it is applied to.

---
+++ Assets/Scripts/Status Effects/Slow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slow : MonoBehaviour
+{
+	CombatStats cs;
+
+	float duration;
+	float potency;
+
+	IEnumerator slowCoroutine()
+	{
+		cs.bonusMovementSpeed -= potency;
+		float remainingDuration = duration;
+		while (remainingDuration > 0)
+		{
+			yield return null;
+			remainingDuration -= Time.deltaTime;
+		}
+		cs.bonusMovementSpeed += potency;
+	}
+
+	public void Initialise(float _potency, float _duration)
+	{
+		cs = GetComponent<CombatStats>();
+		potency = _potency;
+		duration = _duration;
+		StartCoroutine(slowCoroutine());
+	}
+}
