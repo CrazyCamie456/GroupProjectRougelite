e2845b7d3d654c4c4459c915315281732a48aa42
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-02-06 22:23:05 +0000

    Added Basic Enemy
    Added a basic enemy for debug purposes. It will try and path towards the player.

---
+++ Assets/Scripts/Enemy Scripts/DebugEnemyMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugEnemyMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Rigidbody2D rbPlayer;
+
+
+    private Vector2 movementDirection;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //Get the player rigidbody
+        rbPlayer = GameObject.Find("Player").GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //move towards the player
+        movementDirection = rbPlayer.position - rb.position;
+        movementDirection.Normalize();
+        rb.velocity = movementDirection*2;
+    }
+}
