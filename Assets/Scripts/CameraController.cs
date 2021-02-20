e8327849bac61e53a4121cd0052571959b30e8ad
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-20 13:39:01 +0000

    Implementing doors
    Implemented a script that allows rooms to be moved between by the player by passing through a door object, additionally, the camera will follow the player to the new room.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public Vector2 targetPosition;
+	public float movementSpeed;
+
+	void Update()
+    {
+		transform.position = Vector2.Lerp(transform.position, targetPosition, Time.deltaTime * movementSpeed);
+		transform.position = new Vector3(transform.position.x, transform.position.y, -10.0f);
+    }
+}
