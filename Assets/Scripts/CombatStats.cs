a48a90dbb3da03c57052acd28a27b2e4d278554e
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-20 18:05:23 +0000

    Update CombatStats.cs
    Fixed a merge conflict that github didn't feel like showing me.

--- Assets/Scripts/CombatStats.cs
+++ Assets/Scripts/CombatStats.cs
@@ -43,33 +43,6 @@ public class CombatStats : MonoBehaviour
 		return Guid.NewGuid();
 	}
 
-	// Base attack damage and attack speed should be managed by the specific attack,
-	// this means an enemy with multiple attack options could have different base damage or speed.
-	public float bonusAttackDamage;
-	public float bonusAttackSpeed;
-
-	// Stores and makes available whether the object is crowd controlled.
-	private List<Guid> crowdControlList;
-	public bool isCrowdControlled
-	{
-		get
-		{
-			return crowdControlList.Count > 0;
-		}
-	}
-	public void ApplyCrowdControl(Guid crowdControlID)
-	{
-		crowdControlList.Add(crowdControlID);
-	}
-	public void RemoveCrowdControl(Guid crowdControlID)
-	{
-		crowdControlList.Remove(crowdControlID);
-	}
-	// Just a helper function, centralizing the creation of Crowd Controls. Feel free to just use the NewGuid() function in your own code.
-	public Guid NewCrowdControlID()
-	{
-		return Guid.NewGuid();
-	}
 
 	public virtual void TakeDamage(int damage)
 	{
