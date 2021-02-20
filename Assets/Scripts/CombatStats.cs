337276fcf5278514b9e71c8fcf1ad9e74bf253ce
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-20 13:37:32 +0000

    Implementing Knockback on Hit
    Implementing the script that actually applies a knockback to the target on collision with them, plus adding a crowd control.
    Additionally pushing some prefabs for these attacks.

--- Assets/Scripts/CombatStats.cs
+++ Assets/Scripts/CombatStats.cs
@@ -6,38 +6,55 @@ using UnityEngine;
 // All "bonus" values should be as a multiplier, eg 0.2f is a 20% increase to the relative stats, a negative value is a debuff.
 public class CombatStats : MonoBehaviour
 {
+
 	public int maxHealth;
 	// Ensure the entity can never be healed to above their maximum health.
-	public int currentHealth
-	{
-		get
-		{
-			return pCurrentHealth;
-		}
-		set => pCurrentHealth = Mathf.Min(value, maxHealth);
-	}
-	private int pCurrentHealth;
+	public int currentHealth { get; protected set; }
 
 	public float baseMovementSpeed;
 	public float bonusMovementSpeed;
 	public float movementSpeed { get { return Mathf.Max(baseMovementSpeed * (1 + bonusMovementSpeed), 0.0f); } }
-
-	public void TakeDamage(int damage)
-	{
-		currentHealth -= damage;
-		if (currentHealth < 0)
-			Destroy(gameObject);
-	}
 
 	// Base attack damage and attack speed should be managed by the specific attack,
 	// this means an enemy with multiple attack options could have different base damage or speed.
 	public float bonusAttackDamage;
 	public float bonusAttackSpeed;
 
+	// Stores and makes available whether the object is crowd controlled.
+	private List<Guid> crowdControlList;
+	public bool isCrowdControlled
+	{
+		get
+		{
+			return crowdControlList.Count > 0;
+		}
+	}
+	public void ApplyCrowdControl(Guid crowdControlID)
+	{
+		crowdControlList.Add(crowdControlID);
+	}
+	public void RemoveCrowdControl(Guid crowdControlID)
+	{
+		crowdControlList.Remove(crowdControlID);
+	}
+	// Just a helper function, centralizing the creation of Crowd Controls. Feel free to just use the NewGuid() function in your own code.
+	public Guid NewCrowdControlID()
+	{
+		return Guid.NewGuid();
+	}
+
+	public virtual void TakeDamage(int damage)
+	{
+		currentHealth -= damage;
+		if (currentHealth <= 0)
+			Destroy(gameObject);
+	}
+
 	void Start()
 	{
 		// Health can start as a non-full value, but if it is unset, default to max health.
 		if (currentHealth == 0)
 			currentHealth = maxHealth;
+		crowdControlList = new List<Guid>();
 	}
 }
