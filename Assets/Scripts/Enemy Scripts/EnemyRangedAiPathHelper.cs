a2e8263e85b93f8d97416fc4c0a414c04b798082
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-02-21 19:57:02 +0000

    Added a Ranged AI Script
    Added a new ranged AI script that allows for an enemy to move to a certain distance from the player and to have line of sight with the player.

---
+++ Assets/Scripts/Enemy Scripts/EnemyRangedAiPathHelper.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyRangedAiPathHelper : MonoBehaviour
+{
+
+    GameObject player;
+    private AIPath aiPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");   
+        aiPath = GetComponent<AIPath>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 start = transform.position;
+        Vector3 destination = player.transform.position;
+        Vector3 direction = destination - start;
+        Vector3 directionNormalised = direction.normalized;
+        float directionMagnitude = Vector3.Magnitude(direction);
+
+        if (directionMagnitude < 6.0f)
+        {
+        RaycastHit2D ray = Physics2D.RaycastAll(start, directionNormalised, directionMagnitude)[1];
+
+
+                Debug.Log(ray.collider);
+
+
+            
+                Debug.DrawRay(start, directionNormalised * directionMagnitude, Color.red);
+
+            if (ray.collider.tag == "Wall")
+            {
+                aiPath.endReachedDistance = 0.2f;
+            }
+            else
+            {
+                aiPath.endReachedDistance = 6.0f;
+
+            }
+        }
+
+
+
+    }
+}
