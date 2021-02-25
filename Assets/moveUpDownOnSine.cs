e288249ed1225a18e915030ce84edda341f51636
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-25 21:49:16 +0000

    Added sine based movement
    This is used in the first room, when the two spell books are hovering in place.

---
+++ Assets/moveUpDownOnSine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveUpDownOnSine : MonoBehaviour
+{
+	Rigidbody2D rb;
+
+	float startingY;
+	public float yDelta = 1.0f;
+	public float timeScale = 1.0f;
+
+    void Start()
+    {
+		rb = GetComponent<Rigidbody2D>();
+		startingY = rb.position.y;
+    }
+
+    void Update()
+    {
+		rb.position = new Vector2(rb.position.x, startingY + (Mathf.Sin(Time.time * timeScale) * yDelta));
+    }
+}
