fcf3026a8311e72b065e1e8b011aac487f7f9f2a
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-02-26 10:22:36 +0000

    Adding the ability to quit on esc

---
+++ Assets/ExitManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitManager : MonoBehaviour
+{
+	private PlayerController playerController;
+	private void Awake()
+	{
+		playerController = new PlayerController();
+	}
+
+	private void OnEnable()
+	{
+		playerController.Enable();
+	}
+	private void OnDisable()
+	{
+		playerController.Disable();
+	}
+	private void Update()
+	{
+		if (playerController.Player.Escape.ReadValue<float>() > 0.5f)
+			Application.Quit();
+	}
+}
