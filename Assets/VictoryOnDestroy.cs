22296f7f9bc19054b945704b93353946a860c23c
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-03-01 09:55:49 +0000

    Adding scene transition scripts.

---
+++ Assets/VictoryOnDestroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryOnDestroy : MonoBehaviour
+{
+	CombatStats cs;
+
+	private void Start()
+	{
+		cs = GetComponent<CombatStats>();
+	}
+
+	private void OnDestroy()
+	{
+		if (cs.currentHealth <= 0)
+		{
+			SceneManager.LoadScene(2);
+		}
+	}
+}
