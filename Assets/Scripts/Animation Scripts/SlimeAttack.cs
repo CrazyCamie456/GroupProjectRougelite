54d2263c78067b26583a542d45b50efdf0361057
Xinixia <timmy.h.drew@gmail.com>
2021-03-15 12:58:24 +0000

    Added so much stuff
    Several animations including fishmen walking, aggroing & dying, slime moving.
    Added basic lighting & new tiles in first room

---
+++ Assets/Scripts/Animation Scripts/SlimeAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class SlimeAttack : MonoBehaviour
+{
+    Animator anim;
+    private ChargedAttacks charger;
+    private AIPath rb;
+    private SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInParent<AIPath>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        charger = gameObject.GetComponentInParent<ChargedAttacks>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (charger.isCharging)
+            anim.SetTrigger("Attack");
+        else anim.SetTrigger("Idle");
+
+        if (rb.velocity.x > 0)
+            sprite.flipX = true;
+
+        if (rb.velocity.x < 0)
+            sprite.flipX = false;
+    }
+}
