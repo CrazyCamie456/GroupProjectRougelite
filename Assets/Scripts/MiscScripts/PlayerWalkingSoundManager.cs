29091c26533a49f885a8e9e53580730a8c8617f7
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-03-21 20:20:12 +0000

    Introduced Player Foot Steps
    The Player will now make footsteps while moving.

---
+++ Assets/Scripts/MiscScripts/PlayerWalkingSoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkingSoundManager : MonoBehaviour
+{
+    public AK.Wwise.Event wiseevent;
+    public bool isWalking;
+    private float timeBetweenSteps = 0.333333f;
+    private float timer = 0.0f;
+
+    void LateUpdate()
+    {
+        if (isWalking && timer > timeBetweenSteps)
+        {
+            wiseevent.Post(gameObject);
+            timer = 0.0f;
+        }
+
+        timer += Time.deltaTime;
+    }
+}
