21a6e03f0b1bb9cc17760c3c4eec14fe4a0e6c9b
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-03-21 21:02:42 +0000

    Added UI Elements for Equipped Spell
    Added a new UI element for the currently equipped spell

---
+++ Assets/Scripts/UiScripts/EquipedSpell.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipedSpell : MonoBehaviour
+{
+
+    public Sprite fire;
+    public Sprite water;
+    public Sprite earth;
+    public Sprite air;
+    public Image equipedSpell;
+
+    public IElementalAttack elementalAttack;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        elementalAttack = player.GetComponent<IElementalAttack>();
+        string currentlyEquippedSpell = elementalAttack.Name();
+
+        switch (currentlyEquippedSpell)
+        {
+            case "Fire":
+                equipedSpell.enabled = true;
+                equipedSpell.sprite = fire;
+                break;
+            case "Water":
+                equipedSpell.enabled = true;
+                equipedSpell.sprite = water;
+                break;
+            case "Earth":
+                equipedSpell.enabled = true;
+                equipedSpell.sprite = earth;
+                break;
+            case "Air":
+                equipedSpell.enabled = true;
+                equipedSpell.sprite = air;
+                break;
+            default:
+                equipedSpell.enabled = false;
+                break;
+        }
+    }
+}
