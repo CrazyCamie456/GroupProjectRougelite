9f029e33d17e256c05928e25ce7678c9d104592f
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-03-28 19:27:36 +0100

    Added Option Button Functionality
    Added the scene transition functionality to the options button.

---
+++ Assets/Scripts/UiScripts/TitleOptionsButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleOptionsButton : MonoBehaviour
+{
+    public void TaskOnClick()
+    {
+        SceneManager.LoadScene((int)GameScenes.ByID.optionsMenu);
+    }
+}
