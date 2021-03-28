f99bf7c359fb79933539e8db309b97a63f706889
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-03-28 18:27:08 +0100

    Added GameScenes enum
    This should always match up with the scenes in File -> Build Settings, but those scenes can be swapped in and out wherever necessary.

---
+++ Assets/Scripts/Static Scripts/GameScenes.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameScenes
+{
+    enum ByID
+	{
+		titleScreen		 = 0,
+		inGame,			// 1
+		optionsMenu,	// 2
+		deathScreen,	// 3
+		victoryScreen	// 4
+	}
+}
