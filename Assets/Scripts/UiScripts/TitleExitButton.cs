452ecc15c8868132cc255737ce61fc58ccfba1a3
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-03-30 00:22:20 +0100

    Updated the Exit Button
    Updated the exit button to allow the player to quit the game.

--- Assets/Scripts/UiScripts/TitleExitButton.cs
+++ Assets/Scripts/UiScripts/TitleExitButton.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 using UnityEngine.UI;
 public class TitleExitButton : MonoBehaviour
 {
-
+    public static Animator transitioner = GameObject.Find("Crossfade To Black").GetComponentInChildren<Animator>();
+    public static float transitionTime = 1.0f;
     public void TaskOnClick()
     {
 
         StartCoroutine(SceneTransitionFade.LoadNextScene(0));
     }
-
+    public static IEnumerator LoadNextScene(int levelIndex)
+    {
+        transitioner.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        Application.Quit();
+    }
 }
