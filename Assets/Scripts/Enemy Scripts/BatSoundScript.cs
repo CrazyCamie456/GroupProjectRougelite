f468e26f91bb0566cee2c113353b187422043d88
camiecallaghan@gmail.com <camiecallaghan@gmail.com>
2021-03-29 12:38:02 +0100

    Bat sounds if moving

---
+++ Assets/Scripts/Enemy Scripts/BatSoundScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatSoundScript : MonoBehaviour
+{
+	Rigidbody2D rb;
+	CombatStats cs;
+	bool eventPlaying;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+		cs = GetComponent<CombatStats>();
+		eventPlaying = false;
+	}
+
+	void Update()
+	{
+		// If not crowd controlled and has a velocity
+		if (!(cs.isCrowdControlled) && (rb.velocity.x > 0.1f || rb.velocity.x < -0.1f || rb.velocity.y > 0.1f || rb.velocity.y < -0.1f))
+		{
+			if (!eventPlaying)
+			{
+				eventPlaying = true;
+				AkSoundEngine.PostEvent("Play_Bat_Wings", gameObject);
+			}
+		}
+		else
+		{
+			eventPlaying = false;
+			AkSoundEngine.PostEvent("Stop_Bat_Wings", gameObject);
+		}
+	}
+
+	void OnDestroy()
+	{
+		eventPlaying = false;
+		AkSoundEngine.PostEvent("Stop_Bat_Wings", gameObject);
+	}
+}
