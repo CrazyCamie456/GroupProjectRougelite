69bb6734234521e1f321ad922e5670138119ca4e
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-03-29 15:05:03 +0100

    Added Scene transitions to the Player
    Added a Fade to black transition to the player when they die.

---
+++ Assets/Scripts/MiscScripts/CameraSceneTransitionHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSceneTransitionHandler : MonoBehaviour
+{
+    public void PlayerDeathSceneTransition()
+    {
+        StartCoroutine(SceneTransitionFade.LoadNextScene((int)GameScenes.ByID.deathScreen));
+    }
+}
--- Assets/Scripts/UiScripts/TitleOptionsButton.cs
+++ Assets/Scripts/UiScripts/TitleOptionsButton.cs
@@ -6,16 +6,9 @@ using UnityEngine.SceneManagement;
 
 public class TitleOptionsButton : MonoBehaviour
 {
-    public Animator transitioner;
-    public float transitionTime = 1.0f;
     public void TaskOnClick()
     {
-        StartCoroutine(LoadNextScene((int)GameScenes.ByID.optionsMenu));
+        StartCoroutine(SceneTransitionFade.LoadNextScene((int)GameScenes.ByID.optionsMenu));
     }
-    IEnumerator LoadNextScene(int levelIndex)
-    {
-        transitioner.SetTrigger("Start");
-        yield return new WaitForSeconds(transitionTime);
-        SceneManager.LoadScene(levelIndex);
-    }
+
 }
