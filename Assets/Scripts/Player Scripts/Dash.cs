10c2f57b657c5e566e6c30ae689d2579d8621d89
ChefSemiColon <53663699+ChefSemiColon@users.noreply.github.com>
2021-03-29 23:48:24 +0100

    Updated the dash script
    Updated the dash script to include i frames and to go into pits.

--- Assets/Scripts/Player Scripts/Dash.cs
+++ Assets/Scripts/Player Scripts/Dash.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 public class Dash : MonoBehaviour
 {
 
@@ -19,9 +19,10 @@ public class Dash : MonoBehaviour
     private float dashDelay;
 
     private PlayerCombatStats combatStats;
-
     bool notHoldingDash = true;
-
+    bool dashOverPit = false;
+    Tilemap[] tile;
+    private Vector2 dashStartPos;
     private void Awake()
     {
         playerController = new PlayerController();
@@ -34,7 +35,6 @@ public class Dash : MonoBehaviour
     private void OnDisable()
     {
         playerController.Disable();
-
     }
     void Start()
     {
@@ -50,7 +50,7 @@ public class Dash : MonoBehaviour
     {
         //Left shift, space and the right face button
         float dashInput = playerController.Player.Dash.ReadValue<float>();
-
+        tile = FindObjectsOfType<Tilemap>();
         if (dashDelay >= 0.0f)
         {
             dashDelay -= Time.deltaTime;
@@ -65,9 +65,11 @@ public class Dash : MonoBehaviour
         //check if the player has a dash avaliable, is currently not dashing and is trying to dash and is not holding the dash button down from the previous dash
         if (dashLimit > 0 && dashDuration < 0.0f && dashInput > 0.0f && notHoldingDash)
         {
+            dashStartPos = transform.position;
 
-            //Check if the player can dash over a pit
+            dashOverPit = true;
 
+            gameObject.layer = 12;
 
             //while the player dashes disable input
             playerMovement.isDashing = true;
@@ -85,6 +87,23 @@ public class Dash : MonoBehaviour
             notHoldingDash = true;
         }
 
+        //if (playerMovement.isDashing && dashDuration <= 0.0f)
+        //{
+        //    Vector3Int pos = new Vector3Int((int)transform.position.x, (int)transform.position.y, (int)transform.position.z);
+        //    foreach (Tilemap t in tile)
+        //    {
+        //        var name = t.
+        //        name.get
+        //        if (name == null) { }
+        //        //else if (name == "Pit")
+        //        //{
+        //        //    transform.position = dashStartPos;
+        //        //}
+        //    //    Debug.Log("Success!!!maybe?");
+        //    }
+
+        //}
+
         //after the player has dashed resume movement
         if (dashDuration > 0.0f)
         {
@@ -92,7 +111,10 @@ public class Dash : MonoBehaviour
         }
         else
         {
+            gameObject.layer = 0;
+            dashOverPit = false;
             playerMovement.isDashing = false;
         }
+
     }
 }
